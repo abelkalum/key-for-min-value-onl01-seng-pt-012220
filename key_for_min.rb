f631c3375f7ba992488d5afd1637485e3ed5a4d3
Abel Kalum <chumbabel@gmail.com>
2020-01-26 04:32:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
-  hash.collect do |name, value|
+  hash.collect do |name|
     name
   end
  return the name with the smallest value
